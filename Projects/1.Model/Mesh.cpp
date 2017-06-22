5aac257098c4643e9b5b95537939767099bbe60f
seal2002 <hai.phannhut@gameloft.com>
2017-06-22 21:47:30 +0700

    Update Mesh Class

--- Projects/1.Model/Mesh.cpp
+++ Projects/1.Model/Mesh.cpp
@@ -18,10 +18,52 @@ void Mesh::setupMesh()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
 	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+	//vertex positions
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+	//vertex normals
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+	//vertex Textcords
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Texcoords));
+
+	glBindVertexArray(0);
 }
 
 void Mesh::Draw(Shader shader)
 {
+	unsigned int diffuseNr = 1;
+	unsigned int specularNr = 1;
+	for (unsigned int i = 0; i < textures.size(); i++)
+	{
+		glActiveTexture(GL_TEXTURE0 + i); // active proper texture unit before binding
+		// retrieve texture number (the N in diffuse_textureN)
+		stringstream ss;
+		string number;
+		string name = textures[i].type;
+		if (name.compare("texture_diffuse") == 0)
+		{
+			ss << diffuseNr++;
+		}
+		else if (name.compare("texutre_specular") == 0)
+		{
+			ss << specularNr++;
+		}
+		number = ss.str();
+		shader.setFloat(("metarial." + name + number).c_str(), i);
+		glBindTexture(GL_TEXTURE_2D, textures[i].id);
+	}
+	glActiveTexture(GL_TEXTURE0);
+
+	//draw mesh
+	glBindVertexArray(VAO);
+	glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
 }
 
 Mesh::~Mesh()
